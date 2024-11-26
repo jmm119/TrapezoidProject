4ad3cb39455a74a500e03ed8049900b1b2d5af37
jmm119 <jmm119@pct.edu>
2024-11-26 13:19:44 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,11 @@
+from areacalc import calcarea
+from printlib import printarea
+
+
 # Prompt user for three bases
 base1 = float(input("Enter the first base: "))
 base2 = float(input("Enter the second base: "))
-base3 = float(input("Enter the final base: "))
+height = float(input("Enter the height: "))
+
+# Areacalc call
+area = calc(base1, base2, height)
